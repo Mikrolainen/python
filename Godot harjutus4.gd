da0e336e87ea4408d1aed75dcd22e04b89698264
Mirko kohava <115458357+Mikrolainen@users.noreply.github.com>
2023-03-01 15:34:26 +0200

    Update Godot harjutus4.gd

--- Godot harjutus4.gd
+++ Godot harjutus4.gd
@@ -4,7 +4,15 @@ extends Node
 #H4
 
 func _ready():
-	var player =
-	{"Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"}
-	for i in player:
-		print(i)
+	print("--------------------------------------------------------------------")
+	var players = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
+	var size = players.size()
+	var suurim = players.max()
+	players.sort()
+	players.erase("reyes")
+	players.append("Mirko")
+	for player in players:
+		print(player)
+	print("suurim vaartus on: ",suurim)
+	print("mangijate arv on: ",size)
+	print(players[0])
